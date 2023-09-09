5f808c4040a7893f2b052e09a22711be30c298ce
Ryan Edge <ryan.c.edge@outlook.com>
2023-09-08 23:37:12 -0400

    Refactor books route
    Refactor books route to use switch on method type.

--- edge_library_api/routes/books/index.dart
+++ edge_library_api/routes/books/index.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 import 'package:edge_library_common/edge_library_common.dart';
 import 'package:edge_library_data/edge_library_data.dart';
 
 Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    HttpMethod.get => _handleGet(context),
+    _ => Future.value(Response.json(statusCode: HttpStatus.notAcceptable)),
+  };
+}
+
+Future<Response> _handleGet(RequestContext context) async {
   final repository = context.read<BookRepository>();
   final search = context.request.uri.queryParameters['search'];
 
