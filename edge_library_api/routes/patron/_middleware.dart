15f3b9166797e7deab1ed0077ef9b729b6764d5d
Ryan Edge <ryan.c.edge@outlook.com>
2023-09-08 23:10:05 -0400

    Remove redundant middleware

--- edge_library_api/routes/patron/_middleware.dart
+++ edge_library_api/routes/patron/_middleware.dart
@@ -1,13 +1,9 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_auth/dart_frog_auth.dart';
 import 'package:edge_library_data/edge_library_data.dart';
-import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 Handler middleware(Handler handler) {
-  return handler
-      .use(fromShelfMiddleware(corsHeaders()))
-      .use(requestLogger())
-      .use(
+  return handler.use(
     bearerAuthentication<User>(
       authenticator: (context, token) async {
         final userRepository = context.read<AuthenticatorFacade>();
