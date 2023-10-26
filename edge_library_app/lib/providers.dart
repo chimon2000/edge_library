a18dca93200f28371365cf19007d548c35394182
Ryan Edge <ryan.c.edge@outlook.com>
2023-10-25 22:12:03 -0400

    Make currentUserProvider autodispose
    Make currentUserProvider auto dispose to prevent any unwanted caching.

--- edge_library_app/lib/providers.dart
+++ edge_library_app/lib/providers.dart
@@ -3,7 +3,8 @@ import 'package:edge_library_app/shared/extensions/either_option.dart';
 import 'package:passage_flutter/passage_flutter_models/passage_user.dart';
 import 'package:riverpod/riverpod.dart';
 
-final currentUserProvider = FutureProvider<PassageUser?>((ref) async {
+final currentUserProvider =
+    FutureProvider.autoDispose<PassageUser?>((ref) async {
   return (await ref.watch(identityFacadeProvider).checkForAuthenticatedUser())
       .getOrThrow();
 });
